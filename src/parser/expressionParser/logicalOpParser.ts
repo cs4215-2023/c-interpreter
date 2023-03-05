e4e566bfd9b2cb646910632fde6d1a2e2827e123
benluiwj <wenjie.lui@gmail.com>
2023-03-05 10:33:26 +0800

    update logical op parser

--- src/parser/expressionParser/logicalOpParser.ts
+++ src/parser/expressionParser/logicalOpParser.ts
@@ -1,27 +1,31 @@
 import * as es from 'estree'
 
 import { AndContext, OrContext } from '../../lang/ClangParser'
-import { contextToLocation } from '../util'
-import { ExpressionParser } from '.'
+import { Constructable, contextToLocation } from '../util'
 
-export class parserLogicalOpExpression extends ExpressionParser {
-  visitAnd(ctx: AndContext): es.Expression {
-    return {
-      type: 'LogicalExpression',
-      operator: '&&',
-      left: this.visit(ctx._left),
-      right: this.visit(ctx._right),
-      loc: contextToLocation(ctx)
+export const parserLogicalOpExpression = <T extends Constructable>(
+  BaseClass: T
+): typeof DerivedClass => {
+  const DerivedClass = class extends BaseClass {
+    visitAnd(ctx: AndContext): es.Expression {
+      return {
+        type: 'LogicalExpression',
+        operator: '&&',
+        left: this.visit(ctx._left),
+        right: this.visit(ctx._right),
+        loc: contextToLocation(ctx)
+      }
+    }
+
+    visitOr(ctx: OrContext): es.Expression {
+      return {
+        type: 'LogicalExpression',
+        operator: '||',
+        left: this.visit(ctx._left),
+        right: this.visit(ctx._right),
+        loc: contextToLocation(ctx)
+      }
     }
   }
-
-  visitOr(ctx: OrContext): es.Expression {
-    return {
-      type: 'LogicalExpression',
-      operator: '||',
-      left: this.visit(ctx._left),
-      right: this.visit(ctx._right),
-      loc: contextToLocation(ctx)
-    }
-  }
+  return DerivedClass
 }
