04dd1bbc0108cf9c40987d447e745cd9646895ff
benluiwj <wenjie.lui@gmail.com>
2023-03-05 10:32:59 +0800

    update comp parser

--- src/parser/expressionParser/comparatorParser.ts
+++ src/parser/expressionParser/comparatorParser.ts
@@ -9,66 +9,70 @@ import {
   NotEqualContext
 } from '../../lang/ClangParser'
 import { Constructable, contextToLocation } from '../util'
-import { ExpressionParser } from '.'
 
-export class parserComparatorExpression extends ExpressionParser {
-  visitLessThan(ctx: LesserThanContext): es.Expression {
-    return {
-      type: 'BinaryExpression',
-      operator: '<',
-      left: this.visit(ctx._left),
-      right: this.visit(ctx._right),
-      loc: contextToLocation(ctx)
+export const parserComparatorExpression = <T extends Constructable>(
+  BaseClass: T
+): typeof DerivedClass => {
+  const DerivedClass = class extends BaseClass {
+    visitLessThan(ctx: LesserThanContext): es.Expression {
+      return {
+        type: 'BinaryExpression',
+        operator: '<',
+        left: this.visit(ctx._left),
+        right: this.visit(ctx._right),
+        loc: contextToLocation(ctx)
+      }
+    }
+
+    visitLessThanOrEqual(ctx: LesserThanOrEqualContext): es.Expression {
+      return {
+        type: 'BinaryExpression',
+        operator: '<=',
+        left: this.visit(ctx._left),
+        right: this.visit(ctx._right),
+        loc: contextToLocation(ctx)
+      }
+    }
+
+    visitGreaterThan(ctx: GreaterThanContext): es.Expression {
+      return {
+        type: 'BinaryExpression',
+        operator: '>',
+        left: this.visit(ctx._left),
+        right: this.visit(ctx._right),
+        loc: contextToLocation(ctx)
+      }
+    }
+
+    visitGreaterThanOrEqual(ctx: GreaterThanOrEqualContext): es.Expression {
+      return {
+        type: 'BinaryExpression',
+        operator: '>=',
+        left: this.visit(ctx._left),
+        right: this.visit(ctx._right),
+        loc: contextToLocation(ctx)
+      }
+    }
+
+    visitEqualityChecking(ctx: EqualityCheckingContext): es.Expression {
+      return {
+        type: 'BinaryExpression',
+        operator: '==',
+        left: this.visit(ctx._left),
+        right: this.visit(ctx._right),
+        loc: contextToLocation(ctx)
+      }
+    }
+
+    visitNotEqual(ctx: NotEqualContext): es.Expression {
+      return {
+        type: 'BinaryExpression',
+        operator: '!=',
+        left: this.visit(ctx._left),
+        right: this.visit(ctx._right),
+        loc: contextToLocation(ctx)
+      }
     }
   }
-
-  visitLessThanOrEqual(ctx: LesserThanOrEqualContext): es.Expression {
-    return {
-      type: 'BinaryExpression',
-      operator: '<=',
-      left: this.visit(ctx._left),
-      right: this.visit(ctx._right),
-      loc: contextToLocation(ctx)
-    }
-  }
-
-  visitGreaterThan(ctx: GreaterThanContext): es.Expression {
-    return {
-      type: 'BinaryExpression',
-      operator: '>',
-      left: this.visit(ctx._left),
-      right: this.visit(ctx._right),
-      loc: contextToLocation(ctx)
-    }
-  }
-
-  visitGreaterThanOrEqual(ctx: GreaterThanOrEqualContext): es.Expression {
-    return {
-      type: 'BinaryExpression',
-      operator: '>=',
-      left: this.visit(ctx._left),
-      right: this.visit(ctx._right),
-      loc: contextToLocation(ctx)
-    }
-  }
-
-  visitEqualityChecking(ctx: EqualityCheckingContext): es.Expression {
-    return {
-      type: 'BinaryExpression',
-      operator: '==',
-      left: this.visit(ctx._left),
-      right: this.visit(ctx._right),
-      loc: contextToLocation(ctx)
-    }
-  }
-
-  visitNotEqual(ctx: NotEqualContext): es.Expression {
-    return {
-      type: 'BinaryExpression',
-      operator: '!=',
-      left: this.visit(ctx._left),
-      right: this.visit(ctx._right),
-      loc: contextToLocation(ctx)
-    }
-  }
+  return DerivedClass
 }
