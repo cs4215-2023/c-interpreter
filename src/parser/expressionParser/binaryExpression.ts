975ef996f32e8ab493dcd397bf0e6fa6103d8a10
benluiwj <wenjie.lui@gmail.com>
2023-03-05 10:32:19 +0800

    update binary exp parser

--- src/parser/expressionParser/binaryExpression.ts
+++ src/parser/expressionParser/binaryExpression.ts
@@ -7,54 +7,58 @@ import {
   NumberExpressionContext,
   SubtractionContext
 } from '../../lang/ClangParser'
-import { contextToLocation } from '../util'
-import { ExpressionParser } from '.'
+import { Constructable, contextToLocation } from '../util'
 
-export class parserBinaryExpression extends ExpressionParser {
-  visitNumber(ctx: NumberExpressionContext): es.Expression {
-    return {
-      type: 'Literal',
-      value: parseInt(ctx.text),
-      raw: ctx.text,
-      loc: contextToLocation(ctx)
+export const parserBinaryExpression = <T extends Constructable>(
+  BaseClass: T
+): typeof DerivedClass => {
+  const DerivedClass = class extends BaseClass {
+    visitNumber(ctx: NumberExpressionContext): es.Expression {
+      return {
+        type: 'Literal',
+        value: parseInt(ctx.text),
+        raw: ctx.text,
+        loc: contextToLocation(ctx)
+      }
     }
-  }
 
-  visitMultiplication(ctx: MultiplicationContext): es.Expression {
-    return {
-      type: 'BinaryExpression',
-      operator: '*',
-      left: this.visit(ctx._left),
-      right: this.visit(ctx._right),
-      loc: contextToLocation(ctx)
+    visitMultiplication(ctx: MultiplicationContext): es.Expression {
+      return {
+        type: 'BinaryExpression',
+        operator: '*',
+        left: this.visit(ctx._left),
+        right: this.visit(ctx._right),
+        loc: contextToLocation(ctx)
+      }
     }
-  }
-  visitDivision(ctx: DivisionContext): es.Expression {
-    return {
-      type: 'BinaryExpression',
-      operator: '/',
-      left: this.visit(ctx._left),
-      right: this.visit(ctx._right),
-      loc: contextToLocation(ctx)
+    visitDivision(ctx: DivisionContext): es.Expression {
+      return {
+        type: 'BinaryExpression',
+        operator: '/',
+        left: this.visit(ctx._left),
+        right: this.visit(ctx._right),
+        loc: contextToLocation(ctx)
+      }
     }
-  }
-  visitAddition(ctx: AdditionContext): es.Expression {
-    return {
-      type: 'BinaryExpression',
-      operator: '+',
-      left: this.visit(ctx._left),
-      right: this.visit(ctx._right),
-      loc: contextToLocation(ctx)
+    visitAddition(ctx: AdditionContext): es.Expression {
+      return {
+        type: 'BinaryExpression',
+        operator: '+',
+        left: this.visit(ctx._left),
+        right: this.visit(ctx._right),
+        loc: contextToLocation(ctx)
+      }
     }
-  }
 
-  visitSubtraction(ctx: SubtractionContext): es.Expression {
-    return {
-      type: 'BinaryExpression',
-      operator: '-',
-      left: this.visit(ctx._left),
-      right: this.visit(ctx._right),
-      loc: contextToLocation(ctx)
+    visitSubtraction(ctx: SubtractionContext): es.Expression {
+      return {
+        type: 'BinaryExpression',
+        operator: '-',
+        left: this.visit(ctx._left),
+        right: this.visit(ctx._right),
+        loc: contextToLocation(ctx)
+      }
     }
   }
+  return DerivedClass
 }
