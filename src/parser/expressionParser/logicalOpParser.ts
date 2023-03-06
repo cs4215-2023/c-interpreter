f8e433bca66ab5a418232d17ff7cf9130d5ebecb
benluiwj <wenjie.lui@gmail.com>
2023-03-06 11:28:45 +0800

    add xor

--- src/parser/expressionParser/logicalOpParser.ts
+++ src/parser/expressionParser/logicalOpParser.ts
@@ -1,6 +1,6 @@
 import * as es from 'estree'
 
-import { AndContext, OrContext } from '../../lang/ClangParser'
+import { AndContext, OrContext, XorContext } from '../../lang/ClangParser'
 import { Constructable, contextToLocation } from '../util'
 
 export const parserLogicalOpExpression = <T extends Constructable>(
@@ -26,6 +26,17 @@ export const parserLogicalOpExpression = <T extends Constructable>(
         loc: contextToLocation(ctx)
       }
     }
+    visitXor(ctx: XorContext): es.Expression {
+      // use a binary expression here because ^ is not listed as a logical operator
+      return {
+        type: 'BinaryExpression',
+        operator: '^',
+        left: this.visit(ctx._left),
+        right: this.visit(ctx._right),
+        loc: contextToLocation(ctx)
+      }
+    }
   }
+
   return DerivedClass
 }
