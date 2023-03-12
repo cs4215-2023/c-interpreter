834c95e30f933cc4b6b0458c9fd865c683a06513
benluiwj <wenjie.lui@gmail.com>
2023-03-12 10:21:34 +0800

    update type visiting

--- src/parser/expressionParser/identifierParser.ts
+++ src/parser/expressionParser/identifierParser.ts
@@ -1,35 +1,56 @@
-import * as es from 'estree'
+import { Token } from 'antlr4ts/Token'
 
 import {
+  IdentifierExpressionContext,
   IdentifierWithTypeContext,
   TypeContext,
   TypedIdentifierExpressionContext
 } from '../../lang/ClangParser'
-import { PrimitiveValueType, Type } from '../types'
+import { Identifier, PrimitiveValueType, Type } from '../types'
+import { Expression } from '../types'
 import { Constructable } from '../util'
 
 export const parserIdentifierExpression = <T extends Constructable>(
   BaseClass: T
 ): typeof DerivedClass => {
   const DerivedClass = class extends BaseClass {
-    // TODO: update return type
-    visitTypedIdentifierExpression(ctx: TypedIdentifierExpressionContext): es.Expression {
-      console.log('visitTypedIdentifierExpression')
-      const typedIdentifier = this.visitIdentifierWithType(ctx.identifierWithType())
+    tokenToIdentifierWrapper(token: Token): Identifier {
+      if (token.text) {
+        return {
+          type: 'Identifier',
+          name: token.text
+        }
+      }
       return {
         type: 'Identifier',
         name: ''
       }
     }
-    visitIdentifierWithType(ctx: IdentifierWithTypeContext): es.Expression {
+    // TODO: update return type
+    visitTypedIdentifierExpression(ctx: TypedIdentifierExpressionContext): Expression {
+      console.log('visitTypedIdentifierExpression')
+      return this.visitIdentifierWithType(ctx.identifierWithType())
+    }
+
+    visitIdentifierWithType(ctx: IdentifierWithTypeContext): Expression {
       console.log('visitIdentifierWithType')
-      const primitiveType = this.visitType(ctx.type())
+      const type = this.visitType(ctx.type())
+      const identifier = this.tokenToIdentifierWrapper(ctx._id)
       console.log(ctx.IDENTIFIER().text)
       return {
-        type: 'Identifier',
-        name: ''
+        type: 'IdentifierWithTypeExpression',
+        identifier: identifier,
+        identifierType: type
       }
     }
+
+    visitIdentifier(ctx: IdentifierExpressionContext): Expression {
+      return {
+        type: 'Identifier',
+        name: ctx.IDENTIFIER().text
+      }
+    }
+
     visitType(ctx: TypeContext): Type {
       console.log('visitType')
       console.log(ctx.text)
@@ -40,5 +61,6 @@ export const parserIdentifierExpression = <T extends Constructable>(
       }
     }
   }
+
   return DerivedClass
 }
