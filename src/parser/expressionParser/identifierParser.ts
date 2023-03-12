e0c70f2eafbf67d2892c765213f321890e13d34c
benluiwj <wenjie.lui@gmail.com>
2023-03-12 21:18:26 +0800

    remove token wrapper

--- src/parser/expressionParser/identifierParser.ts
+++ src/parser/expressionParser/identifierParser.ts
@@ -1,4 +1,3 @@
-import { Token } from 'antlr4ts/Token'
 import * as es from 'estree'
 
 import {
@@ -7,25 +6,12 @@ import {
   TypedIdentifierExpressionContext
 } from '../../lang/ClangParser'
 import { TypeParser } from '../typeParser'
-import { Identifier } from '../types'
 import { Constructable } from '../util'
 
 export const parserIdentifierExpression = <T extends Constructable>(
   BaseClass: T
 ): typeof DerivedClass => {
   const DerivedClass = class extends BaseClass {
-    tokenToIdentifierWrapper(token: Token): Identifier {
-      if (token.text) {
-        return {
-          type: 'Identifier',
-          name: token.text
-        }
-      }
-      return {
-        type: 'Identifier',
-        name: ''
-      }
-    }
     // TODO: update return type
     visitTypedIdentifierExpression(ctx: TypedIdentifierExpressionContext): es.Expression {
       console.log('visitTypedIdentifierExpression')
