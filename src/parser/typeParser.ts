c8da570436420cf812eb58d63c9a022ad4ad4488
benluiwj <wenjie.lui@gmail.com>
2023-03-13 12:16:19 +0800

    fix type parser

--- src/parser/typeParser.ts
+++ src/parser/typeParser.ts
@@ -1,10 +1,7 @@
 import { AbstractParseTreeVisitor } from 'antlr4ts/tree/AbstractParseTreeVisitor'
 import { ErrorNode } from 'antlr4ts/tree/ErrorNode'
 import { ParseTree } from 'antlr4ts/tree/ParseTree'
-import { RuleNode } from 'antlr4ts/tree/RuleNode'
-import { TerminalNode } from 'antlr4ts/tree/TerminalNode'
 
-import { TypeContext } from '../lang/ClangParser'
 import { ClangVisitor } from '../lang/ClangVisitor'
 import { FatalSyntaxError } from './errors'
 import { PrimitiveValueType, Type } from './types'
@@ -19,16 +16,11 @@ export class TypeParser extends AbstractParseTreeVisitor<Type> implements ClangV
   }
 
   visit(ctx: ParseTree): Type {
-    console.log(ctx.text)
-    return ctx.accept(this)
-  }
-
-  visitChildren(node: RuleNode): Type {
-    return this.defaultResult()
-  }
-
-  visitTerminal(node: TerminalNode): Type {
-    return node.accept(this)
+    return {
+      type: 'PrimitiveType',
+      signed: undefined,
+      valueType: ctx.text as PrimitiveValueType
+    }
   }
 
   visitErrorNode(node: ErrorNode): Type {
@@ -45,21 +37,5 @@ export class TypeParser extends AbstractParseTreeVisitor<Type> implements ClangV
       },
       `invalid syntax ${node.text}`
     )
-  }
-
-  visitPrimitiveType(ctx: TypeContext): Type {
-    return {
-      type: 'PrimitiveType',
-      signed: undefined,
-      valueType: ctx.text as PrimitiveValueType
-    }
-  }
-
-  visitFunctionType(ctx: TypeContext): Type {
-    return {
-      type: 'PrimitiveType',
-      signed: undefined,
-      valueType: ctx.text as PrimitiveValueType
-    }
   }
 }
