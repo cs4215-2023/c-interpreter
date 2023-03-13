ac77e416517187c6c7f755257f80d323ab103849
benluiwj <wenjie.lui@gmail.com>
2023-03-13 12:48:19 +0800

    fix type parser

--- src/parser/typeParser.ts
+++ src/parser/typeParser.ts
@@ -1,12 +1,18 @@
 import { AbstractParseTreeVisitor } from 'antlr4ts/tree/AbstractParseTreeVisitor'
 import { ErrorNode } from 'antlr4ts/tree/ErrorNode'
 import { ParseTree } from 'antlr4ts/tree/ParseTree'
+import { RuleNode } from 'antlr4ts/tree/RuleNode'
+import { TerminalNode } from 'antlr4ts/tree/TerminalNode'
 
+import { FunctionContext, TypeContext } from '../lang/ClangParser'
 import { ClangVisitor } from '../lang/ClangVisitor'
 import { FatalSyntaxError } from './errors'
-import { PrimitiveValueType, Type } from './types'
+import { PrimitiveType, PrimitiveValueType, Type } from './types'
 
-export class TypeParser extends AbstractParseTreeVisitor<Type> implements ClangVisitor<Type> {
+export class TypeParser
+  extends AbstractParseTreeVisitor<PrimitiveType>
+  implements ClangVisitor<PrimitiveType>
+{
   protected defaultResult(): Type {
     return {
       type: 'PrimitiveType',
@@ -15,27 +21,12 @@ export class TypeParser extends AbstractParseTreeVisitor<Type> implements ClangV
     }
   }
 
-  visit(ctx: ParseTree): Type {
+  visitType(ctx: TypeContext): PrimitiveType {
+    console.log('prim type')
     return {
       type: 'PrimitiveType',
       signed: undefined,
       valueType: ctx.text as PrimitiveValueType
     }
-  }
-
-  visitErrorNode(node: ErrorNode): Type {
-    throw new FatalSyntaxError(
-      {
-        start: {
-          line: node.symbol.line,
-          column: node.symbol.charPositionInLine
-        },
-        end: {
-          line: node.symbol.line,
-          column: node.symbol.charPositionInLine + 1
-        }
-      },
-      `invalid syntax ${node.text}`
-    )
   }
 }
