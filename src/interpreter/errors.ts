80a473953294efd48d18b0c7097ca2e1045f6054
benluiwj <wenjie.lui@gmail.com>
2023-03-15 14:00:19 +0800

    change error to function

--- src/interpreter/errors.ts
+++ src/interpreter/errors.ts
@@ -1,7 +1,7 @@
 import { RuntimeSourceError } from '../errors/runtimeSourceError'
 import { Context } from '../types'
 
-export const handleRuntimeError = (context: Context, error: RuntimeSourceError): never => {
+export function handleRuntimeError(context: Context, error: RuntimeSourceError): never {
   context.errors.push(error)
   context.runtime.environments = context.runtime.environments.slice(
     -context.numberOfOuterEnvironments
