8438f1208cd8311e6ae42fb28febd714c626d42c
benluiwj <wenjie.lui@gmail.com>
2023-03-15 14:39:36 +0800

    init parser tests

---
+++ src/parser/__tests__/top-level edge cases.ts
@@ -0,0 +1,32 @@
+import { Program } from 'estree'
+
+import createContext from '../../createContext'
+import { Variant } from '../../types'
+import { parse } from '../parser'
+
+const variant = Variant.DEFAULT
+const context = createContext(variant, undefined, undefined)
+
+describe('Top-level edge cases', () => {
+  it('returns an empty Program for the empty string', () => {
+    const code = ''
+    const prog = parse(code, context)
+    const expectedProg: Program = {
+      type: 'Program',
+      sourceType: 'script',
+      body: []
+    }
+    expect(prog).toEqual(expectedProg)
+  })
+
+  it('filters out the empty statement', () => {
+    const code = ';'
+    const prog = parse(code, context)
+    const expectedProg: Program = {
+      type: 'Program',
+      sourceType: 'script',
+      body: []
+    }
+    expect(prog).toEqual(expectedProg)
+  })
+})
