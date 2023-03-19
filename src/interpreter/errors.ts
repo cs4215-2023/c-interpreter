b91ca36eb815eb57afc475759e355df84f55bf2b
benluiwj <wenjie.lui@gmail.com>
2023-03-19 12:07:15 +0800

    remove utils folder

--- src/interpreter/errors.ts
+++ src/interpreter/errors.ts
@@ -1,42 +1,40 @@
+import * as es from 'estree'
+
 import { RuntimeSourceError } from '../errors/runtimeSourceError'
 import { Context } from '../types'
 import { ErrorSeverity, ErrorType } from '../types'
-import * as es from 'estree'
-import Closure from './closure';
+import Closure from './closure'
 
 export class InterpreterError extends RuntimeSourceError {
-  public type = ErrorType.SYNTAX;
-  public severity = ErrorSeverity.ERROR;
-  public constructor(
-    public node: es.Node,
-    public message = 'Unexpected evaluation',
-  ) {
-    super(node);
+  public type = ErrorType.SYNTAX
+  public severity = ErrorSeverity.ERROR
+  public constructor(public node: es.Node, public message = 'Unexpected evaluation') {
+    super(node)
   }
 
   public explain(): string {
-    return this.message;
+    return this.message
   }
 
   public elaborate(): string {
-    return 'There is an evaluation error. This is an internal error - likely, the interpretation of this type of expression is not yet implemented.';
+    return 'There is an evaluation error. This is an internal error - likely, the interpretation of this type of expression is not yet implemented.'
   }
 }
 
 export class DivisionByZeroError extends RuntimeSourceError {
-  public type = ErrorType.RUNTIME;
-  public severity = ErrorSeverity.ERROR;
+  public type = ErrorType.RUNTIME
+  public severity = ErrorSeverity.ERROR
 
   public constructor(public node: es.Node, public message = 'Division by zero') {
-    super(node);
+    super(node)
   }
 
   public explain(): string {
-    return this.message;
+    return this.message
   }
 
   public elaborate(): string {
-    return this.message;
+    return this.message
   }
 }
 
@@ -48,19 +46,15 @@ export function handleRuntimeError(context: Context, error: RuntimeSourceError):
   throw error
 }
 
-export function assertClosure(
-  closure: any,
-  node: es.Node,
-  context: Context,
-): closure is Closure {
+export function assertClosure(closure: any, node: es.Node, context: Context): closure is Closure {
   if (!(closure instanceof Closure)) {
     return handleRuntimeError(
       context,
       new InterpreterError(
         node,
-        'A non-function was called, which should have been caught by the type checker',
-      ),
-    );
+        'A non-function was called, which should have been caught by the type checker'
+      )
+    )
   }
-  return true;
+  return true
 }
