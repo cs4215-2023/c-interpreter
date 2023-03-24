f7cb92cecb949e25c360306a3916c15fc20ce4bb
benluiwj <wenjie.lui@gmail.com>
2023-03-24 22:32:04 +0800

    add function tests

--- src/interpreter/__tests__/functions.test.ts
+++ src/interpreter/__tests__/functions.test.ts
@@ -17,4 +17,50 @@ describe('Functions', () => {
       expect(1).toBe(2)
     }
   })
+
+  it('Function call returns value ', async () => {
+    const code = `
+	int foo(int a) {
+		return 1;
+	}
+	foo(1);`
+    const result = await sourceRunner(code, context)
+    if (result.status == 'finished') {
+      expect(result.value).toBe(1)
+    } else {
+      expect(1).toBe(2)
+    }
+  })
+
+  it('Function reads parameter ', async () => {
+    const code = `
+	int foo(int a) {
+		return a;
+	}
+	foo(2);`
+    const result = await sourceRunner(code, context)
+    if (result.status == 'finished') {
+      expect(result.value).toBe(2)
+    } else {
+      expect(1).toBe(2)
+    }
+  })
+
+  it('Multiple returns in function ', async () => {
+    const code = `
+	int foo(int a) {
+		if (1) {
+			return 2;
+		} else {
+			return 3;
+		}
+	}
+	foo(2);`
+    const result = await sourceRunner(code, context)
+    if (result.status == 'finished') {
+      expect(result.value).toBe(2)
+    } else {
+      expect(1).toBe(2)
+    }
+  })
 })
