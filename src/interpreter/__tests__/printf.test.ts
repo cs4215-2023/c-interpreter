d6819b98dfd5ca37377ba07fe4681cf56b4e3677
benluiwj <wenjie.lui@gmail.com>
2023-03-29 21:51:33 +0800

    add printf tests

--- src/interpreter/__tests__/printf.test.ts
+++ src/interpreter/__tests__/printf.test.ts
@@ -15,4 +15,29 @@ describe('printf tests', () => {
     const result = printf('%f', 1.0)
     expect(result).toBe('1.000000')
   })
+
+  it('printf with int formatter, float input', async () => {
+    const result = printf('%d', 1.0)
+    expect(result).toBe('1')
+  })
+
+  it('printf with int formatter, char input', async () => {
+    const result = printf('%d', 'a')
+    expect(result).toBe('97')
+  })
+
+  it('printf with char formatter, int input', async () => {
+    const result = printf('%c', 97)
+    expect(result).toBe('a')
+  })
+
+  it('printf with multiple formatters of the same kind', async () => {
+    const result = printf('h%dllo w%drld', 3, 0)
+    expect(result).toBe('h3llo w0rld')
+  })
+
+  it('printf with multiple formatters of different kinds', async () => {
+    const result = printf('h%dllo w%drl%c', 3, 0, 'd')
+    expect(result).toBe('h3llo w0rld')
+  })
 })
