aca4a6f07e97479cde2e79f0c981fce97068c975
Lester Leong <llester.leong@gmail.com>
2023-04-07 10:11:41 +0800

    clean up memorybuffer

--- src/memory/memoryBuffer.ts
+++ src/memory/memoryBuffer.ts
@@ -23,48 +23,11 @@ export default class MemoryBuffer {
     return view
   }
 
-  public mem_set_forwarding_address = (node: number, address: number) =>
-    this.memoryView.setInt32(node * this.word_size, address)
-
-  public mem_get_forwarding_address = (node: number) =>
-    this.memoryView.getInt32(node * this.word_size)
-
   // get and set a word in heap at given address
   public mem_get = (address: number) => this.memoryView.getFloat32(address)
 
   public mem_set = (address: number, x: number) => this.memoryView.setFloat32(address, x)
 
-  // child index starts at 0
-  public mem_get_child = (address: number, child_index: number) =>
-    this.mem_get(address + 1 + child_index)
-
-  public mem_set_child = (address: number, child_index: number, value: number) =>
-    this.mem_set(address + 1 + child_index, value)
-
-  public mem_get_tag = (address: number) => this.memoryView.getInt8(address * this.word_size)
-
-  public mem_get_size = (address: number) =>
-    this.memoryView.getUint16(address * this.word_size + this.size_offset)
-  // the number of children is one less than the size
-  // except for number nodes:
-  //                 they have size 2 but no children
-  public mem_get_number_of_children = (address: number) =>
-    this.mem_get_tag(address) === TAGS.int_tag ? 0 : this.mem_get_size(address) - 1
-
-  // access byte in heap, using address and offset
-  public mem_set_byte_at_offset = (address: number, offset: number, value: number) =>
-    this.memoryView.setUint8(address * this.word_size + offset, value)
-
-  public mem_get_byte_at_offset = (address: number, offset: number) =>
-    this.memoryView.getUint8(address * this.word_size + offset)
-
-  public is_int = (address: number) => this.mem_get_tag(address) === TAGS.int_tag
-
-  public is_char = (address: number) => this.mem_get_tag(address) === TAGS.char_tag
-
-  public is_void = (address: number) => this.mem_get_tag(address) === TAGS.void_tag
-
-  public is_float = (address: number) => this.mem_get_tag(address) === TAGS.int_tag
 
   public address_to_JS_value = (x: number) => {
     return this.mem_get(x) //this is fine for now
