7859cef3f6f472b4e8ddcfa8733ae8724e266933
benluiwj <wenjie.lui@gmail.com>
2023-04-11 19:30:50 +0800

    remove unused function

--- src/typeChecker/utils.ts
+++ src/typeChecker/utils.ts
@@ -1,29 +1,10 @@
 import { typeMappings } from '../constants'
 import * as errors from '../errors/errors'
 import { handleRuntimeError } from '../interpreter/errors'
-import { TypedIdentifier } from '../parser/types'
 import { Node } from '../parser/types'
 import { Context, TypeEnvironment } from '../types'
 import { currentTypeEnvironment } from './typeEnvironment'
 import { ClosureType } from './types'
-
-export function setFunctionParams(
-  node: Node,
-  context: Context,
-  name: string,
-  params: TypedIdentifier[]
-) {
-  let environment: TypeEnvironment | null = currentTypeEnvironment(context)
-  while (environment) {
-    if (environment.head.hasOwnProperty(name)) {
-      environment.head[name] = params
-      return
-    } else {
-      environment = environment.tail
-    }
-  }
-  return handleRuntimeError(context, new errors.UndefinedVariable(name, context.runtime.nodes[0]))
-}
 
 export function assignFunctionType(
   node: Node,
